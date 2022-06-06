93c365ee5660779cbe2099669b23837c1d7fe2f3
gflj <gflj1011@163.com>
2022-06-06 16:20:28 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
         
 #    return render(request,'home.html',{
 #        'new_item_text': new_item_text,
 #    })
-    items = Item.objects.all()
-    return render(request, 'home.html',{'items':items})
+#     items = Item.objects.all()
+    return render(request, 'home.html')
+
+def view_list(request, list_id):
+    # item = Item.objects.all()
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html',{'items':list_})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
